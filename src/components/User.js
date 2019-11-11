36bc3db8fd30843e1cd934827c910a2db651b246
Janne Lahtinen <54285341+jajulaht@users.noreply.github.com>
2019-11-11 19:32:40 +0200

    yksittäisen käyttäjän näkymä done

--- src/components/User.js
+++ src/components/User.js
@@ -2,17 +2,27 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const User = (props) => {
+  const blogFromState = (id) => {
+    const blogFound = props.blogs.find(b => b.id === id)
+    return blogFound.title
+  }
+
   console.log('user component', props.user)
   if (props.user === undefined) {
     return <div>Loading...</div>
   }
   else {
+    const rows = () => props.user.blogs.map(blog =>
+      <li key={blog.id}>
+        { blogFromState(blog.id) }
+      </li>
+    )
     return (
       <div>
         <h3>{props.user.name}</h3>
         <h4>Added blogs</h4>
         <ul>
-          <li></li>
+          {rows()}
         </ul>
       </div>
     )
